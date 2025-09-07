c5e211f78026f624eb9437668480f6bf0bc5e24e
ScorpioOffi <117345081+ScorpioOffi@users.noreply.github.com>
2025-09-07 19:36:56 +0200

    start map level 1

--- Assets/Scripts/Player/PlayerMovement..cs
+++ Assets/Scripts/Player/PlayerMovement..cs
@@ -4,24 +4,36 @@ public class PlayerMovement : MonoBehaviour
 {
   public float speed = 5f;
 
+  public bool gravityEnabled = false;
+  public float gravityScaleWhenEnabled = 1f;
+
+
   private Rigidbody2D rb;
   private Vector2 movement;
 
   void Start()
   {
     rb = GetComponent<Rigidbody2D>();
+
+    rb.freezeRotation = true;
+    rb.gravityScale = gravityEnabled ? gravityScaleWhenEnabled : 0f;
   }
 
   void Update()
   {
-
     movement.x = Input.GetAxisRaw("Horizontal");
     movement.y = Input.GetAxisRaw("Vertical");
+
+    if (movement.sqrMagnitude > 1f)
+      movement = movement.normalized;
+
+    float targetGravity = gravityEnabled ? gravityScaleWhenEnabled : 0f;
+    if (!Mathf.Approximately(rb.gravityScale, targetGravity))
+      rb.gravityScale = targetGravity;
   }
 
   void FixedUpdate()
   {
-
     rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
   }
 
